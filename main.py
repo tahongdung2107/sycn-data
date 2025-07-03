c4c23c89c20418cdd46447a35b9d5dad36fb63d8
Dungth <dungth@kgvietnam.com>
2025-07-03 15:27:36 +0700

    update code

--- main.py
+++ main.py
@@ -16,8 +16,15 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+# Biến trạng thái kiểm soát job quan trọng
+is_critical_job_running = False
+
 def sync_orders_and_bills():
     """Job đồng bộ orders và bills mỗi 15 phút"""
+    global is_critical_job_running
+    if is_critical_job_running:
+        logger.info("Bỏ qua job đồng bộ orders và bills vì đang có job quan trọng chạy!")
+        return
     try:
         logger.info(f"Bắt đầu đồng bộ orders và bills lúc {datetime.now().strftime('%H:%M:%S')}...")
         order_service = OrderService()
@@ -30,6 +37,8 @@ def sync_orders_and_bills():
 
 def sync_categories_and_products():
     """Job đồng bộ categories và products mỗi ngày lúc 01:00"""
+    global is_critical_job_running
+    is_critical_job_running = True
     try:
         logger.info(f"Bắt đầu đồng bộ categories và products lúc {datetime.now().strftime('%H:%M:%S')}...")
         category_service = CategoryService()
@@ -39,9 +48,13 @@ def sync_categories_and_products():
         logger.info("Đồng bộ categories và products hoàn tất!")
     except Exception as e:
         logger.error(f"Lỗi khi đồng bộ categories và products: {str(e)}")
+    finally:
+        is_critical_job_running = False
 
 def sync_delete_and_reload_orders_bills():
     """Job xóa và reload orders, bills cho 2 tháng gần nhất lúc 00:00"""
+    global is_critical_job_running
+    is_critical_job_running = True
     try:
         logger.info(f"Bắt đầu xóa và reload orders, bills lúc {datetime.now().strftime('%H:%M:%S')}...")
         order_service = OrderService()
@@ -62,6 +75,8 @@ def sync_delete_and_reload_orders_bills():
         logger.info("Đã xóa và reload orders, bills cho 2 tháng gần nhất!")
     except Exception as e:
         logger.error(f"Lỗi khi xóa và reload orders, bills: {str(e)}")
+    finally:
+        is_critical_job_running = False
 
 def main():
     try:
@@ -69,7 +84,7 @@ def main():
         schedule.every(15).minutes.do(sync_orders_and_bills)
         
         # Lên lịch chạy job đồng bộ categories và products mỗi ngày lúc 01:00
-        schedule.every().day.at("23:00").do(sync_categories_and_products)
+        schedule.every().day.at("8:00").do(sync_categories_and_products)
 
         # Lên lịch chạy job xóa và reload orders, bills lúc 00:00 mỗi ngày
         schedule.every().day.at("00:00").do(sync_delete_and_reload_orders_bills)
